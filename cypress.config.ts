6e3106b2835393c544cfe241103180d4f16e9cf7
unknown <ernestojuarezgutierrez03@gmail.com>
2024-09-16 22:41:29 -0400

    Segundo Intento

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  env: {
+    USER: "68763",
+    PASSWORD: "Soylaleche2708",
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
